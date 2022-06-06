c6e6fa7ca5b0da705953854fdb706984aa8e1896
rami-nava <raminava60@gmail.com>
2022-06-06 01:24:32 -0300

    TP hasta punto 5

---
+++ 02-FasesErrores/hello6.c
@@ -0,0 +1,6 @@
+int printf(const char * restrict s, ...);//Declaracion
+//Declaracion + definicion
+int main(void){
+ int i=42; //Declaracion y definicion
+ printf("La respuesta es %d\n",i); //Sentencia
+}
